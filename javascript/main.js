86ee3a5a9230611f6f2bdc6724a9eb43d1e7af0e
jimefarias <tu-correo-electronico>
2023-07-03 02:16:05 -0300

    PreEntrega2-GabrielFarias

--- javascript/main.js
+++ javascript/main.js
@@ -11,15 +11,15 @@ else {
 
 let turnos = prompt("ingresa que dia queres tu turno");
 
-while (turnos != "1"){
+while (turnos == 1 || turnos == 2 || turnos == 18); {
     switch (turnos) {
-        case "1":
+        case 1 :
             alert("disponible");
             break;
-        case "2":
+        case 2:
             alert("disponible");
             break;
-        case "18":
+        case 18:
             alert("disponible");
             break;
         default:
@@ -33,12 +33,12 @@ while (turnos != "1"){
     turnos = prompt("proba otro dia")
 }
 let hora = prompt("ingresa el horario")
-while(hora != "14"){
+while(hora == 14 || hora == 16); {
 switch (hora) {
-    case "14":
+    case 14:
         alert("disponible");
         break;
-    case "16":
+    case 16:
         alert("disponible");
         break;
     default:
@@ -53,3 +53,27 @@ alert("su turno es el dia" + " "+ turnos+" "+ "a las" + " " + hora+ "horas")
 
 
 
+
+class Clienta {
+    constructor(nombres,edad,color){
+    this.nombres = nombres;
+    this.edad = edad;
+    this.color = color;
+}
+}
+
+const clienta1 = new Clienta("Andrea", "65", "9.4");
+const clienta2 = new Clienta("Ivana", "26", "6.3");
+const clienta3 = new Clienta("Sofia", "40", "7");
+
+const clientas =[clienta1, clienta2, clienta3];
+
+clientas.push ("clienta4");
+console.log (clientas)
+
+clientas.forEach(clientas => {
+    console.log("nombres:", clientas.nombres);
+    console.log("edad:", clientas.edad);
+    console.log("color:", clientas.color);
+    console.log("----------");
+  });
